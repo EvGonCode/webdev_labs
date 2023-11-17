75adfb75195749f1d00c189d24a7a3095bb417da
Honcharov Yevhen <40567209+EvGonCode@users.noreply.github.com>
2023-11-17 12:23:59 +0200

    Add files via upload

--- Lab_5/main.js
+++ Lab_5/main.js
@@ -5,3 +5,49 @@ document.getElementById('swapButton').addEventListener('click', function () {
     leftArticle.innerHTML = rightArticle.innerHTML;
     rightArticle.innerHTML = temp;
 });
+
+document.getElementById("calculatebutton").addEventListener("click", function() {
+    var widthRadius = document.getElementById("inputField1").value;
+    var heightRadius = document.getElementById("inputField2").value;
+
+    if (widthRadius !== "" && heightRadius !== "") {
+        var ovalSquare = Math.PI * widthRadius * heightRadius;
+        document.getElementById("textField").value = ovalSquare.toFixed(2);
+    } else {
+        alert("Please enter both width and height radius.");
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const rightArticle = document.getElementById('ra');
+
+    rightArticle.addEventListener('dblclick', function () {
+        const currentAlignItems = window.getComputedStyle(rightArticle).alignItems;
+        const newAlignItems = currentAlignItems === 'center' ? 'flex-start' : 'center';
+
+        rightArticle.style.alignItems = newAlignItems;
+        localStorage.setItem('alignItems', newAlignItems);
+    });
+
+    const storedAlignItems = localStorage.getItem('alignItems');
+    if (storedAlignItems) {
+        rightArticle.style.alignItems = storedAlignItems;
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const leftArticle = document.getElementById('la');
+
+    leftArticle.addEventListener('dblclick', function () {
+        const currentJustifyContent = window.getComputedStyle(leftArticle).justifyContent;
+        const newJustifyContent = currentJustifyContent === 'center' ? 'flex-start' : 'center';
+
+        leftArticle.style.justifyContent = newJustifyContent;
+        localStorage.setItem('justifyContent', newJustifyContent);
+    });
+
+    const storedJustifyContent = localStorage.getItem('justifyContent');
+    if (storedJustifyContent) {
+        leftArticle.style.justifyContent = storedJustifyContent;
+    }
+});
