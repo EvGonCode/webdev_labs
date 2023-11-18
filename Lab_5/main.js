6bf08f3329adcb663040b2c77430b20e981b973a
Honcharov Yevhen <40567209+EvGonCode@users.noreply.github.com>
2023-11-18 21:44:49 +0200

    Add files via upload

--- Lab_5/main.js
+++ Lab_5/main.js
@@ -1,3 +1,5 @@
+const wordsAmountCName = "wordsAmount";
+
 document.getElementById('swapButton').addEventListener('click', function () {
     var leftArticle = document.querySelector('.left_article');
     var rightArticle = document.querySelector('.right_article');
@@ -50,4 +52,76 @@ document.addEventListener('DOMContentLoaded', function () {
     if (storedJustifyContent) {
         leftArticle.style.justifyContent = storedJustifyContent;
     }
+});
+
+function setCookie(name, value, daysToLive){
+    const date = new Date();
+    date.setTime(date.getTime() + (daysToLive * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + date.toUTCString();
+    document.cookie = `${name}=${value}; ${expires}; path=/`;
+}
+
+function getCookie(name){
+    const cDecoded = decodeURIComponent(document.cookie);
+    const cArray = cDecoded.split("; ");
+    let result = null;
+
+    cArray.forEach(element =>{
+        if(element.indexOf(name) == 0){
+            result = element.substring(name.length + 1);
+            return result;
+        }
+    })
+    return NaN;
+}
+
+function deleteCookie(name){
+    setCookie(name, null, null);
+}
+
+function resizeWords() {
+    var rightArticleList = document.getElementById("raol");
+    const fieldValue = document.getElementById("inputWords").value;
+    if(!(fieldValue.trim() === "")) {
+        const newWordsAmount = parseInt(fieldValue);
+
+        while (rightArticleList.firstChild) {
+            rightArticleList.removeChild(rightArticleList.firstChild);
+        }
+
+        for (var i = 0; i < newWordsAmount; i++) {
+            var listItem = document.createElement("li");
+            listItem.textContent = `Word`;
+            rightArticleList.appendChild(listItem);
+        }
+
+        setCookie(wordsAmountCName, newWordsAmount, 365);
+    }
+}
+document.addEventListener("DOMContentLoaded", function () {
+
+    var resizeWordsButton = document.getElementById("resizeButton");
+    resizeWordsButton.addEventListener("click", resizeWords);
+});
+document.addEventListener("DOMContentLoaded", function () {
+    var cookieConsentModal = document.getElementById("cookieConsentModal");
+    var acceptCookiesBtn = document.getElementById("acceptCookiesBtn");
+    var rejectCookiesBtn = document.getElementById("rejectCookiesBtn");
+
+    cookieConsentModal.style.display = "block";
+    acceptCookiesBtn.addEventListener("click", function () {
+        localStorage.setItem("hasAcceptedCookies", "true");
+        const newWordsAmount = document.getElementById("inputWords");
+        const cValue = getCookie(wordsAmountCName);
+        if(!isNaN(cValue)){
+            newWordsAmount.value = parseInt(cValue);
+            resizeWords();
+        }
+
+        cookieConsentModal.style.display = "none";
+    });
+
+    rejectCookiesBtn.addEventListener("click", function () {
+        cookieConsentModal.style.display = "none";
+    });
 });
